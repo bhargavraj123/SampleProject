90477bcd4335c7fdfd89fc5383a3f167e8d81290
Bhargav Raj <bhargavraj66@gmail.com>
2023-10-13 20:57:25 +0530

    changes made in Demo.java

--- Demo.java
+++ Demo.java
@@ -1,10 +1,13 @@
 package newjavadevlopment;
 
 public class Demo {
-
+	static void hello{
+		System.out.println("Hello World");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		String y = hello();
+		System.out.println(y);
 	}
 
 }
